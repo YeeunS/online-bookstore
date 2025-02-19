be7dba116b5d7c030222dfb42ce88ce539539680
Yeeun Shin <yeeun2@hawaii.edu>
2025-02-19 12:49:48 +0900

    changed ' as &apos;

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,8 @@ import "@/styles/globals.css";
 export default function Home() {
   return (
     <main>
-      <h1>📚 RGT's Online Bookstore</h1>
-      <p>Welcome to the store!</p>
+      <h1>📚 RGT Online Bookstore</h1>
+      <p>Welcome to the store! It&apos;s great to have you here.</p>
     </main>
   );
 }
